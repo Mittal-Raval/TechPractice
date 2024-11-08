e4559deea74b100a3c609ca82df17e7154e84578
Mittal-Raval <ravalmittal6@gmail.com>
2024-11-08 12:23:06 +0530

    firstquestionCode

---
+++ test1.js
@@ -0,0 +1,50 @@
+function spiralOrder(matrix) {
+
+    let result = [];
+
+    if (matrix.length === 0 || matrix[0].length === 0) return result;
+
+    let r1 = 0, r2 = matrix.length - 1;
+    let c1 = 0, c2 = matrix[0].length - 1;
+
+
+    while  (r1 <= r2 && c1 <= c2) {
+        for (let i = c1; i <= c2; i++){
+            result.push(matrix[r1][i]);
+        }
+        r1++;
+
+        for (let i = r1; i <= r2; i++){
+            result.push(matrix[i][c2]);
+        }
+        c2--;
+
+        if (r1 <= r2) {
+            for (let i = c2; i >= c1; i--){
+                result.push(matrix[r2][i]);
+            }
+            r2--;
+        }
+
+        if ( c1 <= c2) {
+            for (let i = r2; i >= r1; i--){
+                result.push(matrix[i][c1]);
+            }
+            c1++;
+        }
+    }
+
+    return result.join('');
+}
+
+
+let matrix = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+];
+
+let output = spiralOrder(matrix);
+
+console.log(output);
